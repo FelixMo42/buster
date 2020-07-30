3d9a772b04bb0d11bb9a53782076686a5dbfa13c
FelixMo42 <felix@tacocat.com>
2020-07-29 19:15:40 -0700

    finished simple func call ast

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,83 @@
-mod document;
+// mod document;
 
-pub enum NodeKind {
-    MakeFunc,
-    CallFunc,
-    Variable,
-    Sequence,
-    Terminal,
-}
+use std::iter::Iterator;
 
 pub enum Node <'a> {
-    FuncMake (&'a FuncMake<'a>),
-    FuncCall (&'a FuncCall<'a>),
-    Variable (&'a Variable)
+    FuncMake (FuncMake<'a>),
+    FuncCall (FuncCall<'a>),
+    Variable (Variable)
 }
 
 pub struct FuncMakeParam <'a> {
     name : String,
-    expt : Node<'a>
+    kind : Node<'a>
 }
 
 pub struct FuncMake <'a> {
     params : Vec<FuncMakeParam<'a>>,
-    output : Node<'a>
+    output : &'a Node<'a>
 }
 
-pub struct FuncCallArg <'a> {
-    name  : Option<String>,
-    value : Node<'a>
-}
+// pub struct FuncCallArg <'a> {
+//     name  : Option<String>,
+//     value : Node<'a>
+// }
 
 pub struct FuncCall <'a> {
-    func : Node<'a>,
-    args : Vec<FuncCallArg<'a>>
+    func : &'a Node<'a>,
+    args : Vec<Node<'a>>
 }
 
 pub struct Variable {
     name : String
 }
 
-fn encode(node: &Node) -> String {
-    
-    return String::from("")
+fn encode_list<'a, T>(arr: &Vec<T>, func: fn(&T) -> String) -> String {
+    arr.iter().map(func).collect::<Vec<String>>().join(", ")
+}
+
+fn encode<'a>(node: &'a Node<'a>) -> String {
+    match node {
+        Node::FuncMake (a) => format!("([{}]) {}",
+            encode_list(&a.params, |arg| format!("{} {}", encode(&arg.kind), arg.name)),
+            encode(a.output),
+        ),
+        Node::FuncCall (a) => format!("{}({})",
+                encode(a.func),
+                encode_list(&a.args, |arg| encode(&arg))
+            ),
+        Node::Variable (a) => format!("{}", a.name),
+    }
+}
+
+fn var<'a>(name: &'a str) -> Node {
+    return Node::Variable( Variable { name : String::from(name) } )
 }
 
 fn main() {
-    let token = & Variable { name : String::from("hi") };
+    let func = var("+");
 
     let root = FuncMake {
-        params : vec! [],
-        output : Node::Variable( token )
+        params : vec! [
+            FuncMakeParam {
+                name : String::from("a"),
+                kind : var("i64")
+            },
+            FuncMakeParam {
+                name : String::from("b"),
+                kind : var("i64")
+            }
+        ],
+        output : & Node::FuncCall( FuncCall {
+            func : &func,
+            args : vec! [
+                var("a"),
+                var("b"),
+            ]
+        } )
+        
+        // Variable { name : String::from("hi") } )
     };
 
-    println!("{}", encode( & Node::FuncMake( & root ) ));
+    println!("{}", encode( & Node::FuncMake( root ) ));
 }
